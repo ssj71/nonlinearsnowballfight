24d0dc913a4fd93ee4c56ee8e059f075608650aa
Spencer <spencer.jackson@iconfitness.com>
2018-11-07 09:31:01 -0700

    added a snowball, maybe

--- Player.gd
+++ Player.gd
@@ -1,11 +1,8 @@
-extends AnimatedSprite
+extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var speed = 100
-var vel = Vector2()
-
+# Declare member variables here.
+var speed = 200
+var hold = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,18 +10,31 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_key_pressed(KEY_SPACE):
+		hold = hold + delta
+	elif hold != 0:
+		#var ball = RigidBody2D
+		#var sp = Sprite
+		#ball.x = position.x
+		#ball.y = position.y
+		#sp.
+		#ball.add_child( Sprite )
+		#get_parent().add_child(ball)
+		hold = 0
+	var vel = Vector2(0, 0)
 	if Input.is_key_pressed(KEY_RIGHT):
-		vel = Vector2(speed, 0)
-		flip_h = false
-	elif Input.is_key_pressed(KEY_LEFT):
-		vel = Vector2(-speed, 0)
-		flip_h = true
-	elif Input.is_key_pressed(KEY_UP):
-		vel = Vector2(0, -speed)
-	elif Input.is_key_pressed(KEY_DOWN):
-		vel = Vector2(0, speed)
-	else:
-		animation = "stand"
+		vel.x = 1
+		$AnimatedSprite.flip_h = false
+	if Input.is_key_pressed(KEY_LEFT):
+		vel.x = -1
+		$AnimatedSprite.flip_h = true
+	if Input.is_key_pressed(KEY_UP):
+		vel.y = -1
+	if Input.is_key_pressed(KEY_DOWN):
+		vel.y = 1
+	if vel == Vector2(0,0):
+		$AnimatedSprite.animation = "stand"
 		return
-	animation = "run"
-	position = position + vel * delta
+	$AnimatedSprite.animation = "run"
+
+	position = position + speed * vel.normalized() * delta
